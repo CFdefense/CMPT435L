026da8179019b8db9d72eeca91a14b0ccb0b844c
CFdefense <152815652+CFdefense@users.noreply.github.com>
2024-05-23 13:34:41 -0400

    started assignment2

---
+++ CPP/Assignment2/HeaderFiles/Sorts.hpp
@@ -0,0 +1,11 @@
+#ifndef SORTS_HPP
+#define SORTS_HPP
+
+#include <string>
+#include <iostream>
+
+class Sorts {
+    static void insertionSort(std::string arr[], int size);
+};
+
+#endif
---
+++ CPP/Assignment2/Sorts.cpp
@@ -0,0 +1,23 @@
+#include "C:\Users\CFdef\Documents\GitHub\LeetCode\CPP\Assignment2\HeaderFiles\Sorts.hpp"
+
+void Sorts::insertionSort(std::string arr[], int arrSize) {
+    //1st always good loop through all
+    int numComparisons = 0;
+    for(int i = 1; i < arrSize; i++) {
+        int j = i-1; //j will be our pointer to compare our current element to, starts at the one before current
+        std::string curr = arr[i];
+        //Scooch while current element is smaller
+        while(j >= 0) {
+            numComparisons++;
+            if(curr < arr[j]) {
+                arr[j+1] = arr[j];
+                j--;
+            } else {
+                break;
+            }
+        }
+        //place curr element in its spot
+        arr[j+1] = curr;
+    }
+    std::cout << "Insertion Sort Comparisons: " << numComparisons << std::endl;
+};
